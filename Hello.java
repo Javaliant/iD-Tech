649ff20cfec7dc75eaab7a4593d94e4e18c70d52
Luigi Vincent <luigivincenthr@gmail.com>
2015-06-09 02:01:15 -0400

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -5,49 +5,65 @@ Enter the name and click the button (or press enter), to get a greeting.
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Hello extends Application {
-	Scene scene;
+    private Scene scene;
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-	@Override
-	public void start(Stage stage) {
-		TextField entry = new TextField();
-		entry.setPromptText("Enter your name");
+    @Override
+    public void start(Stage stage) {
+        Label nameLabel = new Label("Name:");
+        nameLabel.setStyle("-fx-font-weight: bold;");
 
-		Button hello = new Button("Greeting");
-		hello.setOnAction(e -> {
-			greet(entry);
-			stage.setTitle("Welcome to Java");
-		});
-		hello.setDefaultButton(true);
-		hello.setTooltip(new Tooltip("Click me!"));
+        TextField entry = new TextField();
+        entry.setPromptText("Enter your name");
+        entry.requestFocus();
 
-		HBox layout = new HBox();
-		layout.getChildren().addAll(entry, hello);
-		layout.setPadding(new Insets(2, 12, 2, 12));
+        HBox layout = new HBox(5);
 
-		scene = new Scene(layout);
+        Button hello = new Button("Greeting");
+        hello.setOnAction(e -> {
+            greet(layout, entry);
+            stage.setTitle("Welcome to Java");
+        });
+        hello.setDefaultButton(true);
 
-		stage.setScene(scene);
-		stage.setTitle("Good morning");
-		stage.show();
-		hello.requestFocus();
-	}
+        layout.getChildren().addAll(nameLabel, entry, hello);
+        layout.setPadding(new Insets(2, 12, 2, 12));
+        layout.setAlignment(Pos.CENTER);
 
-	public void greet(TextField entry) {
-		String name = entry.getText();
-		String greeting = name.isEmpty() ? "Hey camper!" : "How's it going, " + name + "?";
-		scene.setRoot(new Label(greeting));
-	}
+        scene = new Scene(layout);
+        stage.setScene(scene);
+        stage.setTitle("Good morning");
+        stage.show();
+    }
+
+    public void greet(HBox layout, TextField entry) {
+        String name = entry.getText();
+        Label greeting = new Label();
+        greeting.setText(name.isEmpty() ? "Hey camper!" : "How's it going, " + name + "?");
+
+        Button reset = new Button("reset");
+        reset.setOnAction(e -> {
+            scene.setRoot(layout);
+        });
+        reset.setTooltip(new Tooltip("Click me!"));
+
+        HBox result = new HBox(5);
+        result.setAlignment(Pos.CENTER);
+        result.getChildren().addAll(greeting, reset);
+        scene.setRoot(result);
+    }
 }
